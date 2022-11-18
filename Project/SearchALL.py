fc4f81fb46512433501cfe02afb6773c4156a6ae
Hyun-woo <nikel4610@gmail.com>
2022-11-18 14:34:52 +0900

    crawling end

--- Project/SearchALL.py
+++ Project/SearchALL.py
@@ -1,23 +1,14 @@
-from SearchKeywords import fm_search1, fm_search2, fm_search3, fm_search4, fm_search5, fm_search6
+from SearchKeywords import fm_search1, fm_search2, fm_search3
 from multiprocessing import Process
-# TODO 실행 시키고 자기
+
 if __name__ == "__main__":
     th1 = Process(target=fm_search1)
     th2 = Process(target=fm_search2)
     th3 = Process(target=fm_search3)
-    th4 = Process(target=fm_search4)
-    th5 = Process(target=fm_search5)
-    th6 = Process(target=fm_search6)
 
     th1.start()
     th2.start()
     th3.start()
-    th4.start()
-    th5.start()
-    th6.start()
     th1.join()
     th2.join()
     th3.join()
-    th4.join()
-    th5.join()
-    th6.join()
--- Project/SearchKeywords.py
+++ Project/SearchKeywords.py
@@ -13,14 +13,14 @@ def fm_search1():
     options = EdgeOptions()
     options.use_chromium = True
     options.add_experimental_option("prefs", {
-        "download.default_directory": r"D:\vsc_project\machinelearning_study\Project\searchData\Data\Keywords",
+        "download.default_directory": r"D:\vsc_project\machinelearning_study\Project\searchData\Data\Keywords7",
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
     })
     driver = Edge('D:/vsc_project/machinelearning_study/edgedriver_win64/msedgedriver.exe', options=options)
     driver.get('https://keywordsound.com/service/keyword-analysis')
-    for i in range(1823, 7523):  # 1788, 1789
+    for i in range(21900, 22278):  # 1980
         driver.find_element_by_xpath(
                             '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/input').send_keys(df_dict[i])
 
@@ -44,14 +44,14 @@ def fm_search2():
     options = EdgeOptions()
     options.use_chromium = True
     options.add_experimental_option("prefs", {
-        "download.default_directory": r"D:\vsc_project\machinelearning_study\Project\searchData\Data\Keywords2",
+        "download.default_directory": r"D:\vsc_project\machinelearning_study\Project\searchData\Data\Keywords8",
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
     })
     driver = Edge('D:/vsc_project/machinelearning_study/edgedriver_win64/msedgedriver.exe', options=options)
     driver.get('https://keywordsound.com/service/keyword-analysis')
-    for i in range(7523, 13223):  # 1788, 1789
+    for i in range(24246, 24623):  # 1985
         driver.find_element_by_xpath(
                             '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/input').send_keys(df_dict[i])
 
@@ -75,107 +75,14 @@ def fm_search3():
     options = EdgeOptions()
     options.use_chromium = True
     options.add_experimental_option("prefs", {
-        "download.default_directory": r"D:\vsc_project\machinelearning_study\Project\searchData\Data\Keywords3",
+        "download.default_directory": r"D:\vsc_project\machinelearning_study\Project\searchData\Data\Keywords9",
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
         "safebrowsing.enabled": True
     })
     driver = Edge('D:/vsc_project/machinelearning_study/edgedriver_win64/msedgedriver.exe', options=options)
     driver.get('https://keywordsound.com/service/keyword-analysis')
-    for i in range(13223, 18923):  # 1788, 1789
-        driver.find_element_by_xpath(
-                            '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/input').send_keys(df_dict[i])
-
-        driver.find_element_by_xpath(
-                            '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/button').click()
-        time.sleep(5)  # 15초대기
-
-        # 날짜 선택
-        driver.find_element_by_xpath('//*[@id="inputDateRange"]').click()
-        # 직접 선택 클릭
-        driver.find_element_by_xpath('//*[@id="kt_body"]/div[3]/div[1]/ul/li[4]').click()
-
-        # 다운로드 클릭
-        driver.find_element_by_xpath('//*[@id="btnExportExcel"]/span').click()
-
-        # 되돌아가기
-        driver.back()
-        driver.find_element_by_xpath(
-                            '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/input').clear()
-def fm_search4():
-    options = EdgeOptions()
-    options.use_chromium = True
-    options.add_experimental_option("prefs", {
-        "download.default_directory": r"D:\vsc_project\machinelearning_study\Project\searchData\Data\Keywords4",
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "safebrowsing.enabled": True
-    })
-    driver = Edge('D:/vsc_project/machinelearning_study/edgedriver_win64/msedgedriver.exe', options=options)
-    driver.get('https://keywordsound.com/service/keyword-analysis')
-    for i in range(18923, 24623):  # 1788, 1789
-        driver.find_element_by_xpath(
-                            '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/input').send_keys(df_dict[i])
-
-        driver.find_element_by_xpath(
-                            '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/button').click()
-        time.sleep(5)  # 15초대기
-
-        # 날짜 선택
-        driver.find_element_by_xpath('//*[@id="inputDateRange"]').click()
-        # 직접 선택 클릭
-        driver.find_element_by_xpath('//*[@id="kt_body"]/div[3]/div[1]/ul/li[4]').click()
-
-        # 다운로드 클릭
-        driver.find_element_by_xpath('//*[@id="btnExportExcel"]/span').click()
-
-        # 되돌아가기
-        driver.back()
-        driver.find_element_by_xpath(
-                            '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/input').clear()
-def fm_search5():
-    options = EdgeOptions()
-    options.use_chromium = True
-    options.add_experimental_option("prefs", {
-        "download.default_directory": r"D:\vsc_project\machinelearning_study\Project\searchData\Data\Keywords5",
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "safebrowsing.enabled": True
-    })
-    driver = Edge('D:/vsc_project/machinelearning_study/edgedriver_win64/msedgedriver.exe', options=options)
-    driver.get('https://keywordsound.com/service/keyword-analysis')
-    for i in range(24623,30323):  # 1788, 1789
-        driver.find_element_by_xpath(
-                            '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/input').send_keys(df_dict[i])
-
-        driver.find_element_by_xpath(
-                            '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/button').click()
-        time.sleep(5)  # 15초대기
-
-        # 날짜 선택
-        driver.find_element_by_xpath('//*[@id="inputDateRange"]').click()
-        # 직접 선택 클릭
-        driver.find_element_by_xpath('//*[@id="kt_body"]/div[3]/div[1]/ul/li[4]').click()
-
-        # 다운로드 클릭
-        driver.find_element_by_xpath('//*[@id="btnExportExcel"]/span').click()
-
-        # 되돌아가기
-        driver.back()
-        driver.find_element_by_xpath(
-                            '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/input').clear()
-def fm_search6():
-    options = EdgeOptions()
-    options.use_chromium = True
-    options.add_experimental_option("prefs", {
-        "download.default_directory": r"D:\vsc_project\machinelearning_study\Project\searchData\Data\Keywords6",
-        "download.prompt_for_download": False,
-        "download.directory_upgrade": True,
-        "safebrowsing.enabled": True
-    })
-    driver = Edge('D:/vsc_project/machinelearning_study/edgedriver_win64/msedgedriver.exe', options=options)
-    driver.get('https://keywordsound.com/service/keyword-analysis')
-    for i in range(30323, len(df_dict)):  # 1788, 1789
+    for i in range(33250, 33625):  # 1969
         driver.find_element_by_xpath(
                             '//*[@id="kt_content_container"]/div/div/div[1]/div[2]/div/div/div/div/input').send_keys(df_dict[i])
 
